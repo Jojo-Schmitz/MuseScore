bd1dac06d87b40a422034b789eb32f9984c0e81a
22justinl <justin7gb@outlook.com>
2024-09-27 14:32:05 +0200

    Fix GH#15321: Allow Removal of Spanners from Range Selections
    Backport of #20634, plus fixing some clazy warnings

--- libmscore/select.cpp
+++ libmscore/select.cpp
@@ -17,47 +17,44 @@
 
 #include "global/log.h"
 
-#include "mscore.h"
+#include "accidental.h"
 #include "arpeggio.h"
+#include "articulation.h"
 #include "barline.h"
 #include "beam.h"
 #include "chord.h"
 #include "dynamic.h"
 #include "element.h"
 #include "figuredbass.h"
+#include "fret.h"
 #include "glissando.h"
 #include "hairpin.h"
 #include "harmony.h"
-#include "fret.h"
 #include "hook.h"
 #include "input.h"
-#include "limits.h"
 #include "lyrics.h"
 #include "measure.h"
+#include "mscore.h"
 #include "note.h"
 #include "notedot.h"
 #include "page.h"
+#include "part.h"
 #include "rest.h"
 #include "score.h"
 #include "segment.h"
 #include "select.h"
 #include "sig.h"
-#include "slur.h"
+#include "staff.h"
+#include "stafftext.h"
 #include "stem.h"
 #include "stemslash.h"
-#include "tie.h"
+#include "sticking.h"
 #include "system.h"
-#include "text.h"
+#include "tie.h"
 #include "tremolo.h"
 #include "tuplet.h"
 #include "utils.h"
 #include "xml.h"
-#include "staff.h"
-#include "part.h"
-#include "accidental.h"
-#include "articulation.h"
-#include "stafftext.h"
-#include "sticking.h"
 
 namespace Ms {
 
@@ -487,15 +484,17 @@ void Selection::appendChord(Chord* chord)
             if (note->accidental()) _el.append(note->accidental());
             for (Element* el : note->el())
                   appendFiltered(el);
-            for (NoteDot* dot : note->dots())
+            for (NoteDot* dot : qAsConst(note->dots()))
                   _el.append(dot);
 
             if (note->tieFor() && (note->tieFor()->endElement() != 0)) {
                   if (note->tieFor()->endElement()->isNote()) {
                         Note* endNote = toNote(note->tieFor()->endElement());
                         Segment* s = endNote->chord()->segment();
-                        if (!s || s->tick() < tickEnd())
-                              _el.append(note->tieFor());
+                        if (!s || s->tick() < tickEnd()) {
+                              for (auto seg : note->tieFor()->spannerSegments())
+                                  appendFiltered(seg);
+                              }
                         }
                   }
             for (Spanner* sp : note->spannerFor()) {
@@ -605,10 +604,10 @@ void Selection::updateSelectedElements()
                         }
                   if (e->isChord()) {
                         Chord* chord = toChord(e);
-                        for (Chord* graceNote : chord->graceNotes())
+                        for (Chord* graceNote : qAsConst(chord->graceNotes()))
                               if (canSelect(graceNote)) appendChord(graceNote);
                         appendChord(chord);
-                        for (Articulation* art : chord->articulations())
+                        for (Articulation* art : qAsConst(chord->articulations()))
                               appendFiltered(art);
                         }
                   else {
@@ -634,14 +633,17 @@ void Selection::updateSelectedElements()
             // ignore voltas
             if (sp->isVolta())
                   continue;
-            if (sp->isSlur()) {
+            if (sp->isSlur() || sp->isHairpin()) {
                   // ignore if start & end elements not calculated yet
                   if (!sp->startElement() || !sp->endElement())
                         continue;
-                  if ((sp->tick() >= stick && sp->tick() < etick) || (sp->tick2() >= stick && sp->tick2() < etick))
-                        if (canSelect(sp->startCR()) && canSelect(sp->endCR()))
-                              appendFiltered(sp);     // slur with start or end in range selection
-            }
+                  if ((sp->tick() >= stick && sp->tick() < etick) || (sp->tick2() >= stick && sp->tick2() < etick)) {
+                        if (canSelect(sp->startCR()) && canSelect(sp->endCR())) {
+                              for (auto seg : sp->spannerSegments())
+                                    appendFiltered(seg);  // slur with start or end in range selection
+                              }
+                        }
+                  }
             else if ((sp->tick() >= stick && sp->tick() < etick) && (sp->tick2() >= stick && sp->tick2() <= etick))
                   appendFiltered(sp); // spanner with start and end in range selection
             }
@@ -710,7 +712,7 @@ void Selection::dump()
             case SelState::RANGE:  qDebug("RANGE"); break;
             case SelState::LIST:   qDebug("LIST"); break;
             }
-      for (const Element* e : _el)
+      for (const Element* e : qAsConst(_el))
             qDebug("  %p %s", e, e->name());
       }
 
@@ -842,10 +844,10 @@ QByteArray Selection::staffMimeData() const
       Fraction ticks  = tickEnd() - tickStart();
       int staves = staffEnd() - staffStart();
       if (!MScore::testMode) {
-            xml.stag(QString("StaffList version=\"" MSC_VERSION "\" tick=\"%1\" len=\"%2\" staff=\"%3\" staves=\"%4\"").arg(tickStart().toString()).arg(ticks.toString()).arg(staffStart()).arg(staves));
+            xml.stag(QString("StaffList version=\"" MSC_VERSION "\" tick=\"%1\" len=\"%2\" staff=\"%3\" staves=\"%4\"").arg(tickStart().toString(), ticks.toString()).arg(staffStart()).arg(staves));
             }
       else {
-            xml.stag(QString("StaffList version=\"2.00\" tick=\"%1\" len=\"%2\" staff=\"%3\" staves=\"%4\"").arg(tickStart().toString()).arg(ticks.toString()).arg(staffStart()).arg(staves));
+            xml.stag(QString("StaffList version=\"2.00\" tick=\"%1\" len=\"%2\" staff=\"%3\" staves=\"%4\"").arg(tickStart().toString(), ticks.toString()).arg(staffStart()).arg(staves));
             }
       Segment* seg1 = _startSegment;
       Segment* seg2 = _endSegment;
@@ -1148,7 +1150,7 @@ std::vector<Note*> Selection::noteList(int selTrack) const
                                     continue;
                               Chord* c = toChord(e);
                               nl.insert(nl.end(), c->notes().begin(), c->notes().end());
-                              for (Chord* g : c->graceNotes()) {
+                              for (Chord* g : qAsConst(c->graceNotes())) {
                                     nl.insert(nl.end(), g->notes().begin(), g->notes().end());
                                     }
                               }
