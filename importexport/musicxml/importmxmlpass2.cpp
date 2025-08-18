9631de93ac03663d771ae88bf5375fab50e95585
James Mizen <j.mizen@mu.se>
2025-08-18 20:50:24 +0200

    Fix import of staff scaling & line distance, update staff-size in xml export
    Backport of #29197, commit 4 and 9

--- importexport/musicxml/importmxmlpass2.cpp
+++ importexport/musicxml/importmxmlpass2.cpp
@@ -3118,8 +3118,13 @@ void MusicXMLParserPass2::staffDetails(const QString& partId, Measure* measure)
             else if (_e.name() == "staff-tuning")
                   staffTuning(&stringData);
             else if (_e.name() == "staff-size") {
+                  bool hasScale;
+                  qreal scale = _e.attributes().value("scale").toDouble(&hasScale);
+                  if (!hasScale)
+                        scale = 1.0;
                   const double val = _e.readElementText().toDouble() / 100;
-                  _score->staff(staffIdx)->setProperty(Pid::MAG, val);
+                  _score->staff(staffIdx)->setProperty(Pid::MAG, scale);
+                  _score->staff(staffIdx)->setProperty(Pid::LINE_DISTANCE, val);
                   }
             else
                   skipLogCurrElem();
