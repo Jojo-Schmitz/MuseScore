c3f3015a9b40d61ca9286d4c6096b35da76442bf
Joachim Schmitz <jojo@schmitz-digital.de>
2025-10-02 10:34:14 +0200

    CI: Use builtin curl instead of wget on Windows
    Backport if #30215, commit 2

--- build/ci/windows/setup.bat
+++ build/ci/windows/setup.bat
@@ -21,9 +21,6 @@ IF NOT %TARGET_PROCESSOR_BITS% == 64 (
 where /q git
 IF ERRORLEVEL 1 ( choco install -y git.install )
 
-where /q wget
-IF ERRORLEVEL 1 ( choco install -y wget )
-
 where /q 7z
 IF ERRORLEVEL 1 ( choco install -y 7zip.install )
 
@@ -44,33 +41,33 @@ IF %TARGET_PROCESSOR_BITS% == 32 (
 
 SET "QT_URL=https://s3.amazonaws.com/utils.musescore.org/%Qt_ARCHIVE%"
 
-CALL "wget.exe" -q --show-progress --no-check-certificate "%QT_URL%" -O "%TEMP_DIR%\%Qt_ARCHIVE%"
+CALL "curl.exe" -f -o "%TEMP_DIR%\%Qt_ARCHIVE%" "%QT_URL%"
 CALL "7z" x -y "%TEMP_DIR%\%Qt_ARCHIVE%" "-o%QT_DIR%"
 
 :: Install dependencies
 ECHO "=== Install dependencies ==="
-CALL "wget.exe" -q --show-progress --no-check-certificate "https://s3.amazonaws.com/utils.musescore.org/musescore_dependencies_win32.7z" -O %TEMP_DIR%\musescore_dependencies_win32.7z
+CALL "curl.exe" -f -o %TEMP_DIR%\musescore_dependencies_win32.7z "https://s3.amazonaws.com/utils.musescore.org/musescore_dependencies_win32.7z"
 CALL "7z" x -y %TEMP_DIR%\musescore_dependencies_win32.7z "-o%TEMP_DIR%\musescore_dependencies_win32"
 SET JACK_DIR="C:\Program Files (x86)\Jack"
 XCOPY %TEMP_DIR%\musescore_dependencies_win32\dependencies\Jack %JACK_DIR% /E /I /Y
-SET PATH=%JACK_DIR%;%PATH% 
+SET PATH=%JACK_DIR%;%PATH%
 
-CALL "wget.exe" -q --show-progress --no-check-certificate "https://s3.amazonaws.com/utils.musescore.org/dependencies.7z" -O  %TEMP_DIR%\dependencies.7z
+CALL "curl.exe" -f -o   %TEMP_DIR%\dependencies.7z "https://s3.amazonaws.com/utils.musescore.org/dependencies.7z"
 CALL "7z" x -y %TEMP_DIR%\dependencies.7z "-oC:\musescore_dependencies"
 
 :: breakpad_tools
 ECHO "=== Install breakpad_tools ==="
-CALL "wget.exe" --no-check-certificate "https://s3.amazonaws.com/utils.musescore.org/dump_syms_32.7z" -O %TEMP_DIR%\dump_syms_32.7z
+CALL "curl.exe" -f -o %TEMP_DIR%\dump_syms_32.7z "https://s3.amazonaws.com/utils.musescore.org/dump_syms_32.7z"
 CALL "7z" x -y %TEMP_DIR%\dump_syms_32.7z "-oC:\breakpad_tools"
 
 IF %BUILD_WIN_PORTABLE% == ON (
 ECHO "=== Installing PortableApps.com Tools ==="
 :: portableappslauncher is a vanilla installation of PortableApps.com Launcher https://portableapps.com/apps/development/portableapps.com_launcher
-CALL "wget.exe" --no-check-certificate "https://s3.amazonaws.com/utils.musescore.org/portableappslauncher.zip" -O %TEMP_DIR%\portableappslauncher.zip
+CALL "curl.exe" -f -o  %TEMP_DIR%\portableappslauncher.zip "https://s3.amazonaws.com/utils.musescore.org/portableappslauncher.zip"
 CALL "7z" x -y %TEMP_DIR%\portableappslauncher.zip "-oC:\portableappslauncher"
 
 :: portableappsinstaller is a vanilla installation of PortableApps.com Installer https://portableapps.com/apps/development/portableapps.com_installer
-CALL "wget.exe" --no-check-certificate "https://s3.amazonaws.com/utils.musescore.org/portableappsinstaller.zip" -O %TEMP_DIR%\portableappsinstaller.zip
+CALL "curl.exe" -f -o %TEMP_DIR%\portableappsinstaller.zip "https://s3.amazonaws.com/utils.musescore.org/portableappsinstaller.zip"
 CALL "7z" x -y %TEMP_DIR%\portableappsinstaller.zip "-oC:\portableappsinstaller"
 )
 
