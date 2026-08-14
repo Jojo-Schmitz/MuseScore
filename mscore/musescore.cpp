e6388499f1a1d591a4f1db259f829a5f801f4bb3
Edward Vanden Berghe <evberghe@gmail.com>
2026-08-14 14:52:32 +0200

    Persist playback highlight setting

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -494,6 +494,7 @@ void MuseScore::preferencesChanged(bool fromWorkspace, bool changeUI)
       getAction("repeat")->setChecked(MScore::playRepeats);
       getAction("pan")->setChecked(MScore::panPlayback);
       getAction("follow")->setChecked(preferences.getBool(PREF_APP_PLAYBACK_FOLLOWSONG));
+      getAction("playback-highlight")->setChecked(preferences.getBool(PREF_APP_PLAYBACK_HIGHLIGHT));
       getAction("midi-on")->setChecked(preferences.getBool(PREF_IO_MIDI_ENABLEINPUT));
       getAction("toggle-statusbar")->setChecked(preferences.getBool(PREF_UI_APP_SHOWSTATUSBAR));
       getAction("show-tours")->setChecked(preferences.getBool(PREF_UI_APP_STARTUP_SHOWTOURS));
@@ -6419,6 +6420,8 @@ void MuseScore::cmd(QAction* a, const QString& cmd)
             showContextHelp();
       else if (cmd == "follow")
             preferences.setPreference(PREF_APP_PLAYBACK_FOLLOWSONG, a->isChecked());
+      else if (cmd == "playback-highlight")
+            preferences.setPreference(PREF_APP_PLAYBACK_HIGHLIGHT, a->isChecked());
       else if (cmd == "split-h")
             splitWindow(true);
       else if (cmd == "split-v")
